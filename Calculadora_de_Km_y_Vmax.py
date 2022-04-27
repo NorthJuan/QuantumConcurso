0ea1da617cf9219764b9349787b2eec374a055cd
NorthJuan <102831530+NorthJuan@users.noreply.github.com>
2022-04-26 20:42:31 -0600

    .

--- Calculadora_de_Km_y_Vmax.py
+++ Calculadora_de_Km_y_Vmax.py
@@ -3,5 +3,5 @@ st.title("Calculadora para Km y Vmax")
 st.text("Este proyecto será realizado para observar el comportamiento de los parámetros cinéticos característicos de una enzima (Km) y (Vmax). Esto modificando las concetraciones de sustrato presente.")
 
 st.write("Inserte los valores del sustrato")
-nn1 = st.number_input("Dame n1")
+nn1 = st.number_input("Dame [S1]")
 nn2 = st.number_input("Dame n2")
