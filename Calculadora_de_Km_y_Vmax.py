95bfba82991c357298924701315a5f751bfd6519
NorthJuan <102831530+NorthJuan@users.noreply.github.com>
2022-04-26 21:02:32 -0600

    1

--- Calculadora_de_Km_y_Vmax.py
+++ Calculadora_de_Km_y_Vmax.py
@@ -12,17 +12,10 @@ with col1:
     nn4 = st.number_input("Dame [S4]")
     nn5 = st.number_input("Dame [S5]")
 
-st.write("Inserte las concentraciones de los sustrato [S] (mmol/L)")
-nn1 = st.number_input("Dame [S1]")
-nn2 = st.number_input("Dame [S2]")
-nn3 = st.number_input("Dame [S3]")
-nn4 = st.number_input("Dame [S4]")
-nn5 = st.number_input("Dame [S5]")
-
 with col2:
-st.write("Inserte los valores de las velocidades (mmol/L)min-1)")
-nn6 = st.number_input("Dame [V1]")
-nn7 = st.number_input("Dame [V2]")
-nn8 = st.number_input("Dame [V3]")
-nn9 = st.number_input("Dame [V4]")
-nn10 = st.number_input("Dame [V5]")
+    st.write("Inserte los valores de las velocidades (mmol/L)min-1)")
+    nn6 = st.number_input("Dame [V1]")
+    nn7 = st.number_input("Dame [V2]")
+    nn8 = st.number_input("Dame [V3]")
+    nn9 = st.number_input("Dame [V4]")
+    nn10 = st.number_input("Dame [V5]")
