bb8288dd16cbd2f259a032b1690cbe627a700059
NorthJuan <102831530+NorthJuan@users.noreply.github.com>
2022-04-26 20:15:14 -0600

    Text

--- Calculadora_de_Km_y_Vmax.py
+++ Calculadora_de_Km_y_Vmax.py
@@ -1,2 +1,3 @@
 import streamlit as st
 st.title("Calculadora para Km y Vmax")
+st.text("Este proyecto será realizado para observar el comportamiento de los parámetros cinéticos característicos de una enzima (Km) y (Vmax). Esto modificando las concetraciones de sustrato presente.")
